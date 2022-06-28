75c8339b87aee7a8cd14808067df69271d4b839d
Javier Gomez <javiergomezve@gmail.com>
2022-06-28 14:16:29 -0500

    [ADD] Display partial data from cache

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import { FC } from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createClient, Provider as UrqlProvider } from 'urql';
+import {
+    createClient,
+    dedupExchange,
+    fetchExchange,
+    Provider as UrqlProvider,
+} from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
 
+import schema from './src/graphql/graphql.schema.json';
 import RootNavigator from './src/screens/Root.navigator';
 
 const client = createClient({
-    url: 'https://9228-190-67-220-99.ngrok.io/graphql',
+    url: 'https://0bbb-190-67-220-99.ngrok.io/graphql',
+    exchanges: [
+        dedupExchange,
+        cacheExchange({
+            schema: schema as any,
+            resolvers: {
+                Query: {
+                    story: (_, args) => ({ __typename: 'Story', id: args.id }),
+                },
+            },
+        }),
+        fetchExchange,
+    ],
 });
 
 const App: FC = () => {
